91b7c8d717dac10e687c5eeb215f38d6e977c63e
Martin Alemajoh <alemajohmartin@gmail.com>
2021-10-20 19:33:47 -0400

    added middle to check the state parameter

---
+++ middleware/checkState.js
@@ -0,0 +1,10 @@
+function checkState(req, res, next) {
+  const state = req.body.state;
+  if (state !== process.env.STATE) {
+    next(new Error("Invalid state"));
+  } else {
+    next();
+  }
+}
+
+export default checkState;
