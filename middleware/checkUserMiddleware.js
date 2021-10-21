da80b7596d9477556ca6d6af08feb3c816413378
Martin Alemajoh <alemajohmartin@gmail.com>
2021-10-20 23:38:51 -0400

    implemented checkUserMiddleware

--- middleware/checkUserMiddleware.js
+++ middleware/checkUserMiddleware.js
@@ -1,9 +1,21 @@
 import Errorhandler from "./../utils/Errorhandler.js";
 import Logger from "./../utils/Logger.js";
+import User from "./../model/User.js";
+import { findOne, insertOne, findAndUpdate } from "./../database/query.js";
 
-function checkUserMiddleware(req, res, next) {
+async function checkUserMiddleware(req, res, next) {
   try {
-    next();
+    const user = ({ firstName, lastName, emailAddress, image } = req.body.userProfile);
+    const result = await findOne(User, { emailAddress: emailAddress });
+    if (!result) {
+      const newUser = await insertOne(User, user);
+      req.body.user = newUser;
+      next();
+    } else {
+      const existingUser = await findAndUpdate(User, { emailAddress: emailAddress }, { image: image });
+      req.body.user = existingUser;
+      next();
+    }
   } catch (error) {
     Logger.log("error", error, import.meta.url);
     next(new Errorhandler("Internal server error", 500));
