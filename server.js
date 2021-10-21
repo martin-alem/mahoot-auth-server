5b5b4995504e33ce76b91b889ca0c5c0c2e1838c
Martin Alemajoh <alemajohmartin@gmail.com>
2021-10-20 23:15:03 -0400

    added database connection and implemented some mongodb queries

--- server.js
+++ server.js
@@ -1,14 +1,18 @@
 import dotenv from "dotenv";
 import express from "express";
 import cors from "cors";
+import { connectToSmackDatabase } from "./database/connection.js";
 import linkedinRouter from "./routes/linkedInLoginRoute.js";
 
 dotenv.config();
+
+//connect to smack database
+connectToSmackDatabase();
+
 const app = express();
 
 app.use(cors());
 app.use(express.json());
-
 
 app.use("/api/v1/", linkedinRouter);
 
