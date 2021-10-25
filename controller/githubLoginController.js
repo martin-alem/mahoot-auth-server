c902b89b0b37a6e6806aa270ac44f29d9a1a3ced
Martin Alemajoh <alemajohmartin@gmail.com>
2021-10-25 01:05:28 -0400

    added console logs to track a bug

--- controller/githubLoginController.js
+++ controller/githubLoginController.js
@@ -6,8 +6,8 @@ function githubLoginController(req, res, next) {
   console.log("INSIDE GITHUB LOGIN CONTROLLER");
   try {
     const user = req.body.user;
-    const accessToken = jwt.sign({ user_id: user["_id"] }, process.env.JWT_SECRET, { expiresIn: "1h" });
-    res.cookie("_access_token", `${accessToken}`, { expires: new Date(Date.now() + 1 * 3600000), httpOnly: true });
+    // const accessToken = jwt.sign({ user_id: user["_id"] }, process.env.JWT_SECRET, { expiresIn: "1h" });
+    res.cookie("_access_token", `Martin`, { expires: new Date(Date.now() + 1 * 3600000), httpOnly: true });
     res.status(200).json({ status: "success", statusCode: 200, user_id: user["_id"] });
   } catch (error) {
     Logger.log("error", error, import.meta.url);
