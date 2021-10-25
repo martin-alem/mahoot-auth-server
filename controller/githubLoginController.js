6a0de92237ba4f12bd7ba544519091b84d9603e0
Martin Alemajoh <alemajohmartin@gmail.com>
2021-10-25 07:58:08 -0400

    added trust proxy setting

--- controller/githubLoginController.js
+++ controller/githubLoginController.js
@@ -6,7 +6,7 @@ function githubLoginController(req, res, next) {
   try {
     const user = req.body.user;
     const accessToken = jwt.sign({ user_id: user["_id"] }, process.env.JWT_SECRET, { expiresIn: "1h" });
-    res.cookie("_access_token", `${accessToken}`, { expires: new Date(Date.now() + 1 * 3600000), httpOnly: true, secure: true, sameSite: "Strict" });
+    res.cookie("_access_token", `${accessToken}`, { expires: new Date(Date.now() + 1 * 3600000), httpOnly: true, secure: true, sameSite: "None" });
     console.log("COOKIE VALUE: " + accessToken);
     res.status(200).json({ status: "success", statusCode: 200, user_id: user["_id"] });
   } catch (error) {
