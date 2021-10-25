fed92c6b7dbc095ad2c4a22df0f448a74e33efd1
Martin Alemajoh <alemajohmartin@gmail.com>
2021-10-25 08:15:10 -0400

    added trust proxy setting

--- server.js
+++ server.js
@@ -15,10 +15,12 @@ const app = express();
 var corsOptions = {
   origin: true,
   credentials: true,
+  methods: ["GET", "PUT", "POST", "DELETE", "OPTIONS"],
+  allowedHeaders: ["Content-Type", "Authorization"],
   optionsSuccessStatus: 200, // some legacy browsers (IE11, various SmartTVs) choke on 204
 };
 
-app.set("trust proxy", true);
+app.enable("trust proxy");
 app.use(cors(corsOptions));
 app.use(express.json());
 
